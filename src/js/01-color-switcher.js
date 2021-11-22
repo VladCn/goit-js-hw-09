09a59dd2188ea3f6214694e8b9ea97b42b275692
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-11-22 11:56:48 +0200

    HM 9.1 реализовано функционал кнопок Старт и Стоп

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const start = document.querySelector('button[data-start]')
+const stop = document.querySelector('button[data-stop]')
+const body = document.querySelector('body')
+
+
+start.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor()
+  },1000);
+
+});
+
+function getRandomHexColor() {
+  console.log("sdsd")
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+console.log(getRandomHexColor())
+
+stop.addEventListener("click", () => {
+  clearInterval(timerId);
+});
+
 
