d964366406a1eee7ffb789ff3a6fcf1fe03f81af
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-11-22 15:52:59 +0200

    HM 9.2 сделана разметка на Флексах

---
+++ src/js/02-timer.js
@@ -0,0 +1,15 @@
+// Описан в документации
+import flatpickr from "flatpickr";
+// Дополнительный импорт стилей
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
