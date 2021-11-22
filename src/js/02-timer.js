dc537d5ffbd8a9787ad2b42cc430331bbf8c7c63
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-11-22 16:30:43 +0200

    HM 9.2 сделан оброботчик события при не правильной дате

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,6 +3,7 @@ import flatpickr from "flatpickr";
 // Дополнительный импорт стилей
 import "flatpickr/dist/flatpickr.min.css";
 
+const inputDate = document.querySelector('input[type="text"]')
 
 const options = {
   enableTime: true,
@@ -10,6 +11,16 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    if(selectedDates[0] <= date){
+      window.alert("Please choose a date in the future")
+      return;
+    }
     console.log(selectedDates[0]);
   },
 };
+
+flatpickr(inputDate, options)
+
+const date = new Date();
+
+console.log(date);
