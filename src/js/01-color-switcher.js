f9eb9ba0c6b1d9f938f81063952c784fe90c5960
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-11-22 12:48:35 +0200

    HM 9.1 реализован интервал без дублирования

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,12 +1,20 @@
 const start = document.querySelector('button[data-start]')
 const stop = document.querySelector('button[data-stop]')
 const body = document.querySelector('body')
+let timerId = null
+const TIMEDELAY = 1000;
+let aktive = false;
 
 
-start.addEventListener('click', () => {
+start.addEventListener('click', (evt) => {
+  if(aktive === true ){
+    return
+  }
+  aktive = true;
+  console.log(evt)
   timerId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor()
-  },1000);
+  },TIMEDELAY);
 
 });
 
@@ -14,10 +22,11 @@ function getRandomHexColor() {
   console.log("sdsd")
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-console.log(getRandomHexColor())
+
 
 stop.addEventListener("click", () => {
   clearInterval(timerId);
+  aktive = false;
 });
 
 
