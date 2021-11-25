4f2151115293e9af54582149374e19ae55469687
Vladislav Zaitsev <vladislav.zaytcev84@gmail.com>
2021-11-25 13:37:52 +0200

    HM 9.3 финал, результат через промисы

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,52 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const delayMs = document.querySelector("input[name=delay]")
+const stepMs = document.querySelector("input[name=step]")
+const amount = document.querySelector("input[name=amount]")
+const form = document.querySelector(".form")
+
+delayMs.addEventListener("input", (event) => {
+  delayMs.value = event.currentTarget.value;
+});
+
+stepMs.addEventListener("input", (event) => {
+  stepMs.value = event.currentTarget.value;
+});
+
+amount.addEventListener("input", (event) => {
+  amount.value = event.currentTarget.value;
+});
+
+form.addEventListener('submit', submitHandler)
+
+function submitHandler(event){
+  event.preventDefault();
+  let counter = Number(delayMs.value)
+  for (let i = 0; i < Number(amount.value); i +=1){
+    createPromise(i, counter)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    counter += Number(stepMs.value)
   }
 }
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  console.log(position, delay)
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() =>{
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay)
+
+  })
+
+}
+
+
